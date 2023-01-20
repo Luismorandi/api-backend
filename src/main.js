160b91d4fff8ef0a1a32fc44005d2fa674ed6ecf
luismo <luismmorandit@gmail.com>
2023-01-19 21:40:17 -0300

    second commit

--- src/main.js
+++ src/main.js
@@ -4,18 +4,11 @@ import logger from "./logger/logger.js";
 import {initMongoDB}  from './db/database.js'
 
 const yargs = parse(process.argv.slice(2));
-const { port, _ } = yargs
-  .alias({
-    p: "puerto",
-  })
-  .default({
-    port: 8080,
-  }).argv;
-
+const PORT = process.env.PORT || 8080
 
 const init = async () => {
     await initMongoDB();
-    Server.listen(port, ()=> console.log(`Servidor online en ${port}`));
+    Server.listen(PORT, ()=> console.log(`Servidor online en ${PORT}`));
     Server.on('error', (error) => logger.error(`Error en servidor ${error}`));
 }
 
